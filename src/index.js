80bff32490724bea399fcb9681907f63c7e51d19
sagardas25 <sagar.xyz05@gmail.com>
2025-05-05 12:23:22 +0530

    intial setup and logger

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import app from "./app.js"
+import dotenv from "dotenv"
+import logger from "../logger.js";
+import morgan from "morgan";
+
+
+const morganFormat = ":method :url :status :response-time ms";
+
+
+dotenv.config({
+
+  path : "./.env"
+
+})
+
+
+
+const port = process.env.PORT || 3000
+
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.listen(port, () => {
+  console.log(` app listening on port ${port}`)
+})
