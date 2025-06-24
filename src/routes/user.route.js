27bb90bd06f14aa5e2de3941862f2a3c324878ed
sagardas25 <sagar.xyz05@gmail.com>
2025-06-24 20:26:33 +0530

    created routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,13 +1,27 @@
 import { Router } from "express";
-import { logoutUser, registerUser } from "../controllers/user.controller.js";
+import {
+  registerUser,
+  loginUser,
+  refreshAccessToken,
+  logoutUser,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAcoountDetails,
+  changeUserAvatar,
+  changeUserCoverImage,
+  getWatchHistory,
+  getUserChannelProfile
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-//multur middleware : upload.fields(...) is used to handle the  files. 
+//multur middleware : upload.fields(...) is used to handle the  files.
 //it uploads the files correctly and made the files available in req.body or req.files in the controller
 // after that controller performs the business logics
+
+//unsecured routes
 router.route("/register").post(
   upload.fields([
     {
@@ -23,7 +37,19 @@ router.route("/register").post(
   registerUser
 );
 
+router.route("/login").post(loginUser);
+router.route("/refresh-token").post(refreshAccessToken);
+
 //secured route
 router.route("/logout").post(verifyJwt, logoutUser);
+router.route("/change-password").post(verifyJwt, changeCurrentPassword);
+router.route("/change-password").post(verifyJwt, changeCurrentPassword);
+router.route("/current-user").get(verifyJwt, getCurrentUser);
+router.route("/update-account").patch(verifyJwt, updateAcoountDetails);
+router.route("/ch/:username").get(verifyJwt, getUserChannelProfile);
+router.route("/avatar").patch(verifyJwt,upload.single("avatar") ,changeUserAvatar);
+router.route("/cover-image").patch(verifyJwt,upload.single("coverImage") ,changeUserCoverImage);
+router.route("/history").get(verifyJwt, getWatchHistory);
+
 
 export default router;
